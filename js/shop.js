21d026c2057326d5f90f3e20e4d699d57fbe6c41
Race Freeze <Mathofreeze@gmail.com>
2024-07-12 00:18:23 -0600

    Add A5-4 code.

--- js/shop.js
+++ js/shop.js
@@ -1,22 +1,76 @@
 let cardContainer = document.querySelector("#cardContainer");
 
-function addProducts() {
-    products.forEach(
-        individualCard => cardContainer.innerHTML += 
-        `
-        <div class="card" id="cardNumber${individualCard.id}">
-            <img src="${imagePath}${individualCard.image}" alt="${individualCard.description}">
-            <div class="cardText">
-                <h4>${individualCard.name}</h4>
-                <p>${individualCard.description}</p>
-                <p>&dollar; ${individualCard.price}</p>
-                <button class="cartButton" id="${individualCard.id}">Add to Cart</button>
+// function addProducts() {
+//     products.forEach(
+//         individualCard => cardContainer.innerHTML += 
+//         `
+//         <div class="card" id="cardNumber${individualCard.id}">
+//             <img src="${imagePath}${individualCard.image}" alt="${individualCard.description}">
+//             <div class="cardText">
+//                 <h4>${individualCard.name}</h4>
+//                 <p>${individualCard.description}</p>
+//                 <p>&dollar; ${individualCard.price}</p>
+//                 <button class="cartButton" id="${individualCard.id}">Add to Cart</button>
+//             </div>
+//         </div>
+//         `
+//     );
+// }
+// addProducts();
+
+let productsPerPage = 6;
+let currentPage = 1;
+let pagedResults = [];
+let totalProducts = products.length;
+console.log(((currentPage * productsPerPage) >= totalProducts)? true : false);
+function paginate() {
+    let end = currentPage*productsPerPage;
+    let start = end - productsPerPage;
+    pagedResults = products.slice(start, end);
+    $('#cardContainer').empty();
+    $(pagedResults).each((index, individualCard)=>{
+        $('#cardContainer').append(
+            `
+            <div class="card" id="cardNumber${individualCard.id}">
+                <img src="${imagePath}${individualCard.image}" alt="${individualCard.description}">
+                <div class="cardText">
+                    <h4>${individualCard.name}</h4>
+                    <p>${individualCard.description}</p>
+                    <p>&dollar; ${individualCard.price}</p>
+                    <button class="cartButton" id="${individualCard.id}">Add to Cart</button>
+                </div>
             </div>
-        </div>
-        `
-    );
+            `
+        );
+    });
+    if(currentPage <= 1) {
+        $('.previous').attr('disabled', 'true');
+    } else {
+        $('.previous').removeAttr('disabled');
+    }
+    if((currentPage * productsPerPage) >= totalProducts) {
+        $('.next').attr('disabled', 'true');
+    } else {
+        $('.next').removeAttr('disabled');
+    }
 }
-addProducts();
+paginate();
+
+$('.previous').click(()=>{
+    if(currentPage > 1) {
+        currentPage--;
+        paginate();
+        saveToLocalStorage();
+    }
+});
+
+$('.next').click(()=>{
+    if((currentPage * productsPerPage) <= totalProducts) {
+        currentPage++;
+        paginate();
+        saveToLocalStorage();
+    }
+});
 
 function saveToLocalStorage() {
     let cartButton = document.getElementsByClassName("cartButton");
