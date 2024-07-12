6b04255794e8b43e0d95d48ed48498e25a6ee087
Race Freeze <Mathofreeze@gmail.com>
2024-07-11 21:53:05 -0600

    Add aside functionality.

--- js/app.js
+++ js/app.js
@@ -68,6 +68,20 @@ function updateQuantity() {
             if (typeof displayOrderSummary === "function") {
                 displayOrderSummary();
             }
-        }, false);
+        });
     }
 }
+
+$(function() {
+    let sidebarOrigin = $('aside').offset();
+    $(window).scroll(()=>{
+        let scrollPosition = $('html').scrollTop();
+        if(sidebarOrigin.top < scrollPosition) {
+            $('aside').addClass('sticky');
+            $('aside').css({'width': '17.9%'});
+        } else {
+            $('aside').removeClass('sticky');
+            $('aside').css({'width': '20%'});
+        }
+    })
+});
